a5fbd9a5db3596cc0672a578b4ebfbfc5cd339fe
ardeshir.ahouri <ardeshir.ahouri@gmail.com>
2020-07-06 09:58:09 +0430

    impl slot

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,15 @@
         <keep-alive>
           <component v-bind:is="currentPage"></component>
         </keep-alive>
+        <cc-footer>
+          <p class="text-center">&copy;Creative Cards</p>
+          <ul class="nav justify-content-center">
+            <li class="nav-item"><a class="nav-link">Home</a></li>
+            <li class="nav-item"><a class="nav-link">About us</a></li>
+            <li class="nav-item"><a class="nav-link">Contact us</a></li>
+
+          </ul>
+        </cc-footer>
       </div>
     </div>
   </div>
@@ -17,6 +26,7 @@ import CardFront from "./components/card/CardFront.vue";
 import CardInsideLeft from "./components/card/CardInsideLeft";
 import CardInsideRight from "./components/card/CardInsideRight";
 import CardBack from "./components/card/CardBack";
+import Footer from "./components/Footer";
 
 export default {
   data: function(){
@@ -29,7 +39,8 @@ export default {
     cardFront:CardFront,
     cardInsideLeft:CardInsideLeft,
     cardInsideRight:CardInsideRight,
-    cardBack:CardBack
+    cardBack:CardBack,
+    ccFooter:Footer
   }
 }
 </script>
